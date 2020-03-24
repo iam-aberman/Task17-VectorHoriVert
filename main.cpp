91fd0a0ba71389d4a8237a494f7e3190472953d8
Osip Chin <57560888+iam-aberman@users.noreply.github.com>
2020-03-24 18:47:48 +0300

    Добавлена обработка исключений в main

--- main.cpp
+++ main.cpp
@@ -11,79 +11,84 @@
 
 using namespace std;
 
-using CRat = VectorHoriVert::Vector<Rational>;
-
-using Factory = VectorHoriVert::Factory<Rational>;
-using HoriFactory = VectorHoriVert::HoriFactory<Rational>;
-using VertFactory = VectorHoriVert::VertFactory<Rational>;
+using CRat = VECTOR_HORI_VERT::Vector<Rational>;
+using Factory = VECTOR_HORI_VERT::Factory<Rational>;
+using HoriFactory = VECTOR_HORI_VERT::HoriFactory<Rational>;
+using VertFactory = VECTOR_HORI_VERT::VertFactory<Rational>;
 
 int main() {
     // unit-tests
     TestAll();
 
-    // Task
-    string filename = "test.txt";
-    /*cout << "Enter filename ... ";
-    cin >> filename;*/
+    try {
+        // Task
+        string filename = "test.txt";
+        /* cout << "Enter filename ... ";
+        cin >> filename; */
 
 
-    ifstream inputFile(filename);
-    if (!inputFile) {
-        cerr << "\nError: file not found!" << endl;
+        ifstream inputFile(filename);
+        if (!inputFile) {
+            cerr << "\nError: file not found!" << endl;
+            return -1;
+        }
+
+        vector<shared_ptr<CRat>> vectors_;
+        vector<string> filenames_;
+
+        // factories
+        map<string, shared_ptr<Factory>> factories_ = {
+                {"Hori", make_shared<HoriFactory>()},
+                {"Vert", make_shared<VertFactory>()}
+        };
+
+        string inputString;
+        while (getline(inputFile, inputString)) {
+            stringstream temp(inputString);
+            string type, outputFilename;
+
+            temp >> type >> outputFilename;
+
+            if (factories_.count(type) != 1) {
+                cerr << "Error: invalid Vector type!" << endl;
+                return -1;
+            }
+
+            auto resFactory = factories_[type];
+            auto newVector = resFactory->Create(0u);
+
+            Rational tmpRational;
+            while (temp >> tmpRational) {
+                newVector->PushBack(tmpRational);
+            }
+
+            vectors_.push_back(newVector);
+            filenames_.push_back(outputFilename);
+        }
+
+        inputFile.close();
+
+        auto vectorIt = vectors_.begin();
+        auto fileIt = filenames_.begin();
+
+        while (vectorIt != vectors_.end() && fileIt != filenames_.end()) {
+            ofstream outputFile(*fileIt);
+            if (!outputFile) {
+                cerr << "Error: can not open file " << (*fileIt) << endl;
+                return -1;
+            }
+
+            (*vectorIt)->Print(outputFile);
+
+            ++vectorIt;
+            ++fileIt;
+
+            outputFile.close();
+        }
+    }
+    catch (const exception& e) {
+        cerr << e.what() << endl;
         return -1;
-    }
-
-    vector<shared_ptr<CRat>> vectors_;
-    vector<string> filenames_;
-
-    // factories
-    map<string, shared_ptr<Factory>> factories_ = {
-            {"Hori", make_shared<HoriFactory>()},
-            {"Vert", make_shared<VertFactory >()}
-    };
-
-    string inputString;
-    while (getline(inputFile, inputString)) {
-        stringstream temp(inputString);
-        string type, outputFilename;
-
-        temp >> type >> outputFilename;
-
-        if (factories_.count(type) != 1) {
-            cerr << "Error: invalid Vector type!" << endl;
-            return -1;
-        }
-
-        auto resFactory = factories_[type];
-        auto newVector = resFactory->Create(0u);
-
-        Rational tmpRational;
-        while (temp >> tmpRational) {
-            newVector->PushBack(tmpRational);
-        }
-
-        vectors_.push_back(newVector);
-        filenames_.push_back(outputFilename);
-    }
-
-    inputFile.close();
-
-    auto vectorIt = vectors_.begin();
-    auto fileIt = filenames_.begin();
-
-    while (vectorIt != vectors_.end() && fileIt != filenames_.end()) {
-        ofstream outputFile(*fileIt);
-        if (!outputFile) {
-            cerr << "Error: can not open file " << (*fileIt) << endl;
-            return -1;
-        }
-
-        (*vectorIt)->Print(outputFile);
-
-        ++vectorIt;
-        ++fileIt;
-
-        outputFile.close();
     }
 
     return 0;
